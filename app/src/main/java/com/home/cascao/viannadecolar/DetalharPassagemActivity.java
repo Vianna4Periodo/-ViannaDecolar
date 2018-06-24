9a397451204e2779bb62b3af51c8f2ede9701742
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-06-24 08:57:21 -0300

    Primeiro commit

---
+++ app/src/main/java/com/home/cascao/viannadecolar/DetalharPassagemActivity.java
@@ -0,0 +1,13 @@
+package com.home.cascao.viannadecolar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalharPassagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhar_passagem);
+    }
+}
