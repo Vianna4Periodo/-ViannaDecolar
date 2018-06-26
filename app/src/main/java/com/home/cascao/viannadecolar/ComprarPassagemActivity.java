95f9c0c0a9b1e8239cc18f20d3667fac3d93687f
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-06-26 13:11:49 -0300

    Verificando disponibilidade voo

--- app/src/main/java/com/home/cascao/viannadecolar/ComprarPassagemActivity.java
+++ app/src/main/java/com/home/cascao/viannadecolar/ComprarPassagemActivity.java
@@ -94,18 +94,13 @@ public class ComprarPassagemActivity extends AppCompatActivity {
         repository.comprarPassagem(new VooComprarPassagemRequest(id, Utils.getInstance().getCliente().getId(), quantidade, 200)).enqueue(new Callback<VooComprarPassagemResponse>() {
             @Override
             public void onResponse(Call<VooComprarPassagemResponse> call, Response<VooComprarPassagemResponse> response) {
-                if (response.body().getPassagens() == null || response.body().getPassagens().size() == 0) {
-                    Toast.makeText(getApplicationContext(), "Você não pode ir nesse voo poiso mesmo já está lotado.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Passagem comprada com sucesso.", Toast.LENGTH_SHORT).show();
-                }
-
+                Toast.makeText(getApplicationContext(), "Passagem comprada com sucesso.", Toast.LENGTH_SHORT).show();
                 finish();
             }
 
             @Override
             public void onFailure(Call<VooComprarPassagemResponse> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "Não foi possível comprar, tente novamente mais tarde por favor.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Você não pode ir nesse voo poiso mesmo já está lotado.", Toast.LENGTH_SHORT).show();
             }
         });
     }
